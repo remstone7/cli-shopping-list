94b094ddad917e2609d14e60d62e95d7f20240aa
Rem Stone <remstone7@gmail.com>
2017-07-20 18:00:07 -0400

    initial commit

---
+++ shopping_list.py
@@ -0,0 +1,10 @@
+# make a list that will hold onto our items
+shopping_list = []
+# print out instructions
+print("What should we pick up at the store?")
+print("Enter 'DONE' to stop adding items.")
+# ask for new items
+
+# add new items to our list
+# quit app
+# print out
